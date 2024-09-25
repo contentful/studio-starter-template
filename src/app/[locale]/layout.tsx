8c864470530ef331822d99446d63b6dff9f74cac
caseylisak <casey.lisak@contentful.com>
2024-09-25 13:58:01 -0700

    Cleans up files and updates SDK

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 
-import { Inter } from "next/font/google";
 import "../globals.css";
 import Header from "@components/layout/Header/Header";
 import Footer from "@components/layout/Footer/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Studio Quickstart",
