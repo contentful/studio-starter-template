49110937fa4bf5e2e99b759c73d1b091bf449751
Charlie Chrisman <charlie@contentful.com>
2025-06-28 09:52:04 +0200

    updated to the new convention as of next-intl@3.22

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,6 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { LOCALES } from './lib/constants';
+import { LOCALES } from '../src/lib/constants';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!LOCALES.includes(locale)) {
